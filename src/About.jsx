a71e0d5435ba8e82c5dafb0353ceaad075f630a0
Paul Ezvan <paul@ezvan.fr>
2019-10-05 23:27:13 -0700

    Add about description.

--- src/About.jsx
+++ src/About.jsx
@@ -10,7 +10,12 @@ const About = () => (
     <Container>
       <Row>
         <Col md={{span: 6, offset: 4}}>
-          Wesh
+          <Jumbotron>
+            <h1>About Local Green Tips</h1>
+            <p>Here you can discover and share tips to help the environment specific to where you are.</p>
+            <p>The environment is different wherever you go! As such the best way to preserve varies a lot between locations. Local Green Tips is here to help on the best action you can take to help.</p>
+            <p>If you have tips to share, <NavLink to="/register">register now</NavLink> and <NavLink to="/addtip">write the tip!</NavLink></p>
+          </Jumbotron>
         </Col>
       </Row>
       </Container>

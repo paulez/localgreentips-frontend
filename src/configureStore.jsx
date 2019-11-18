d0526cee26b1c2b2b17bda603c3741c9e6254df1
Paul Ezvan <paul@ezvan.fr>
2019-11-17 18:31:05 -0800

    Persist store.

--- src/configureStore.jsx
+++ src/configureStore.jsx
@@ -1,18 +1,29 @@
-import { createStore, applyMiddleware, compose } from 'redux'
-import thunkMiddleware from 'redux-thunk'
-import { createLogger } from 'redux-logger'
-import rootReducer from './reducers'
+import { createStore, applyMiddleware, compose } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import rootReducer from './reducers';
 
-const loggerMiddleware = createLogger()
+const loggerMiddleware = createLogger();
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default function configureStore(preloadedState) {
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+function configureStore() {
   return createStore(
-    rootReducer,
-    preloadedState,
+    persistedReducer,
     composeEnhancers(
       applyMiddleware(thunkMiddleware, loggerMiddleware)
     )
-  )
+  );
 }
+
+export const store = configureStore();
+export const persistor = persistStore(store);
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import configureStore from './configureStore';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+import { store, persistor } from './configureStore';
 import './index.css';
 import App from './App';
 import Login from './Login';
@@ -12,22 +13,22 @@ import TipPage from './TipPage';
 import { AddTip } from './AddTip';
 import * as serviceWorker from './serviceWorker';
 
-const store = configureStore()
-
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <div>
-        <Route path="/" exact component={App} />
-        <Route path="/login/" component={Login} />
-        <Route path="/register/" component={Register} />
-	<Route path="/about/" component={About} />
-	<Route path="/tip/:index" component={TipPage} />
-	<Route path="/addtip/" component={AddTip} />
-      </div>
-    </Router>
-  </Provider>,
-    document.getElementById('root'));
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Router>
+          <div>
+            <Route path="/" exact component={App} />
+            <Route path="/login/" component={Login} />
+            <Route path="/register/" component={Register} />
+	    <Route path="/about/" component={About} />
+	    <Route path="/tip/:index" component={TipPage} />
+	    <Route path="/addtip/" component={AddTip} />
+          </div>
+        </Router>
+      </PersistGate>
+    </Provider>,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
